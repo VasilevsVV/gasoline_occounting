e1f8412aabe6f3cd9cace8980e5794c88441737f
Natalia <natalia.chehunina@yandex.ua>
2017-04-15 17:13:57 +0300

    Added list_trips methods and delete_trip method.

--- gas_accounting/business_logic.py
+++ gas_accounting/business_logic.py
@@ -23,3 +23,17 @@ class GasolineTable:
         self.trips_table[self.trips_ids] = trip
         self.trips_ids += 1
         return self.trips_ids - 1
+
+    def delete_trip(self,id):
+        return self.trips_table.pop(id)
+
+    def list_trips_between_dates(self,start_date,final_date):
+        return [val for val in self.trips_table.values()
+                if (val.end_date > start_date > val.start_date or
+                    val.end_date > final_date > val.start_date)]
+
+    def list_trips_after_date(self,date):
+        return [val for val in self.trips_table.values() if (val.end_date > date)]
+
+    def list_trips_before_date(self,date):
+        return [val for val in self.trips_table.values() if (val.start_date < date)]
