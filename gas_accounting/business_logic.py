3ae4aa688bb49acb3f2e841e626552662bafa946
Vasiliy <morfey.rulit@gmail.com>
2017-04-15 15:15:15 +0300

    Removed invalid add function.

--- gas_accounting/business_logic.py
+++ gas_accounting/business_logic.py
@@ -9,6 +9,3 @@ class GasolineTable:
         self.trip_table = {}
         self.purchase_ids = 0
         self.trips_ids = 0
-
-    def add_trip(self, start_date, end_date=0):
-        self.trip_table[self.trips_ids] = entities.Trip(start_date, end_date)
