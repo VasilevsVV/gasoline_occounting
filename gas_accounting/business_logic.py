645ebabedc403aee5bd54742ac9caff9af907eef
Vasiliy <morfey.rulit@gmail.com>
2017-04-15 14:40:28 +0300

    Deleted purchaselogic.

--- gas_accounting/business_logic.py
+++ gas_accounting/business_logic.py
@@ -9,12 +9,6 @@ class GasolineTable:
         self.purchase_ids = 0
         self.trips_ids = 0
 
-    def add_purchase(self, fuel, price, date):
-        self.purchase_table[self.purchase_ids] = entities.Purchase(date, fuel, price)
-        self.purchase_ids += 1
-        return self.purchase_ids - 1
-
-    def add_trip(self, start_date, end_date = 0):
+    def add_trip(self, start_date, end_date=0):
         self.trip_table[self.trips_ids] = entities.Trip(start_date, end_date)
-
 
--- gas_accounting/entities.py
+++ gas_accounting/entities.py
@@ -1,5 +1,6 @@
 from gas_accounting import utils
 
+
 class Purchase:
     def __init__(self, date, fuel, price):
         self.date = date
@@ -13,21 +14,16 @@ class Trip:
         self.start_date = start_date
         self.end_date = end_date
         self.fuel = 0
-        self.purchaseList = []
         self.cost = 0
 
     def calc_cost_add(self, fuel, price):
         self.fuel += fuel
-        self.cost += fuel * price
+        self.cost  += fuel * price
         return self.cost
 
     def add_fuel(self, fuel):
         self.fuel += fuel
         return self.fuel
-
-    def add_purchase(self, purchase_id):
-        self.purchaseList.append(purchase_id)
-        return self.purchaseList
 
     # def get_fuel_from_purchases(self):
     #     for p in self.purchaseList:
