a3f35ae6a6e12a52c7e6468476061d63adc79cf3
Vasiliy <morfey.rulit@gmail.com>
2017-04-23 18:22:03 +0300

    Added parse_time functions.

--- gas_accounting/utils.py
+++ gas_accounting/utils.py
@@ -3,3 +3,24 @@ import time
 
 def make_time(year, month, day, hours, minutes):
     return time.mktime((year, month, day, hours, minutes, 0,0,0,0))
+
+def parse_time(string):
+    dots = string.count(":")
+    try:
+        if dots == 4:
+            return time.strptime(string, "%Y:%m:%d:%H:%M")
+        elif dots == 3:
+            return time.strptime(string, "%Y:%m:%d:%H")
+        elif dots == 2:
+            return time.strptime(string, "%Y:%m:%d")
+        else:
+            return "Invalid date format"
+    except Exception as e:
+        return "Error while parsing time:\n\t{}".format(e)
+
+
+def parse_time_with_format(string, format):
+    try:
+        return time.strptime(string, format)
+    except Exception as e:
+        return "{}".format(e)
