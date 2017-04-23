48081a041757ec147dc6add0053db40f1cd9cd20
Vasiliy <morfey.rulit@gmail.com>
2017-04-23 16:51:29 +0300

    Added console user interface.

--- gas_accounting/user_interface.py
+++ gas_accounting/user_interface.py
@@ -2,6 +2,30 @@
 
 from gas_accounting import controller
 
-
 def show_help(category):
     return 1
+
+
+def parse_string(str):
+    str = str.lstrip()
+    lst = str.split()
+    command = lst.pop(0)
+    if(lst and lst[0].startswith("-")):
+        params_str = lst.pop()
+        params = [ch for ch in params_str[1:len(str)]]
+    else:
+        params = []
+    args = lst
+    return [command, params, args]
+
+def start_console():
+    loop_flag = True
+    while(loop_flag):
+        str = input(" > ").strip()
+        if str == "exit":
+            loop_flag = False
+        elif str != "":
+            res = parse_string(str)
+            print(controller.process_console_request(res[0], res[1], res[2]))
+
+start_console()
