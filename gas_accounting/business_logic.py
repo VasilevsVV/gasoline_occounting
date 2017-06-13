abe84ab2dc06aa9e988bf185475c4b8396181770
Natalia <natalia.chehunina@yandex.ua>
2017-06-13 22:03:57 +0300

    added documentations to business_logic.

--- gas_accounting/business_logic.py
+++ gas_accounting/business_logic.py
@@ -1,10 +1,13 @@
 import entities
 import pickle
+import utils
 
 
 class GasolineTable:
-
+    """ Class with main logic.
+    Working with trips."""
     def __init__(self):
+        """Initializes the GasolineTable class."""
         try:
             with open('gas_storage.pickle', 'rb') as f:
                 [self.trips_table, self.trips_ids] = pickle.load(f)
@@ -14,6 +17,7 @@ class GasolineTable:
             self.trips_ids = 0
 
     def __del__(self):
+        """ Saves (dumps) current table when session is over."""
         try:
             with open('gas_storage.pickle', 'wb') as f:
                 pickle.dump([self.trips_table, self.trips_ids], f)
@@ -21,6 +25,11 @@ class GasolineTable:
             print("Error while dumping table!!!\n{}".format(e))
 
     def add_trip(self, start_date, final_date, fuel):
+        """ Adds a trip to table. 
+        Args:
+            start_date(time): Date of trips begin.
+            final_date(time): Date of trips end.
+            fuel(number): Amount of spent fuel."""
         trip = entities.Trip(start_date, final_date)
         trip.fuel = fuel
         self.trips_table[self.trips_ids] = trip
@@ -29,6 +38,12 @@ class GasolineTable:
 
     def add_trip_consumption(self, start_date, final_date,
                              distance, fuel_per_km):
+        """ Adds a trip to table by distance and fuel/km.
+        Args:
+            start_date(time): Date of trips begin.
+            final_date(time): Date of trips end.
+            distance(number): Distance of trip.
+            fuel_per_km(number): fuel/km consumption."""
         trip = entities.Trip(start_date, final_date)
         trip.fuel = fuel_per_km * distance
         self.trips_table[self.trips_ids] = trip
@@ -36,15 +51,28 @@ class GasolineTable:
         return self.trips_ids - 1
 
     def delete_trip(self, id):
+        """Deletes trip by id.
+        Args:
+            id(integer): Id of trip."""
         return self.trips_table.pop(id)
 
     def get_trip_by_id(self, id):
+        """ Returns trip by id.
+        Args:
+            id(integer): Id of trip."""
         return self.trips_table[id]
 
     def list_all(self):
+        """ Returns list of all trips."""
         return [(key, val) for key, val in self.trips_table.items()]
 
     def list_trips_between_dates(self, start_date, final_date, strict=False):
+        """ Returns list of all trips between two dates.
+        Args:
+            start_date(time): minimal date.
+            final_date(time): maximal date.
+            strict(boolean): if false: trips with dates
+             equal to min or max date would be listed."""
         if strict:
             return [(key, val) for key, val in self.trips_table.items()
                     if (val.start_date > start_date and
@@ -55,6 +83,11 @@ class GasolineTable:
                         val.end_date > final_date > val.start_date)]
 
     def list_trips_after_date(self, date, strict=False):
+        """ Return list of all trips after particular date.
+        Args:
+            date(time): minimal date of trip.
+            strict(boolean): if false: trips with dates
+             equal to minimal date would be listed."""
         if strict:
             return [(key, val) for key, val in self.trips_table.items()
                     if (val.start_date > date)]
@@ -63,6 +96,11 @@ class GasolineTable:
                     if (val.end_date >= date)]
 
     def list_trips_before_date(self, date, strict=False):
+        """ Returns list of all trips before particular date.
+        Args:
+            date(time): maximal date of trip.
+            strict(boolean): if false: trips with dates
+             equal to maximal date would be listed."""
         if strict:
             return [(key, val) for key, val in self.trips_table.items()
                     if (val.end_date < date)]
@@ -71,23 +109,46 @@ class GasolineTable:
                     if (val.start_date <= date)]
 
     def search_trips_by_date(self, date):
+        """ Return list of all trips which took place on
+        particular date.
+        Args:
+            date(time): Date of required trips."""
         return [(key, val) for key, val in self.trips_table.items()
                 if (val.end_date >= date >= val.start_date)]
 
     def calculate_gasoline_between_dates(self, start_date, final_date,
                                          strict=False):
+        """ Returns amount of fuel which was spent between 
+        particular dates.
+        Args:
+            start_date(time): minimal date.
+            final_date(time): maximal date.
+            strict(boolean): if false: fuel from trips with dates
+             equal to min or max date would be added to sum."""
         res = 0
         for i in self.list_trips_between_dates(start_date, final_date, strict):
             res += i[1].fuel
         return res
 
     def calculate_gasoline_after_date(self, date, strict=False):
+        """ Returns amount of fuel which was spent after
+        particular date.
+        Args:
+            date(time): minimal date of trips.
+            strict(boolean): if false: fuel from trips with dates
+             equal to minimal date would be added to sum."""
         res = 0
         for i in self.list_trips_after_date(date, strict):
             res += i[1].fuel
         return res
 
     def calculate_gasoline_before_date(self, date, strict=False):
+        """ Returns amount of fuel which was spent after
+                particular date.
+        Args:
+            date(time): maximal date of trips.
+            strict(boolean): if false: fuel from trips with dates 
+                equal to maximal date would be added to sum."""
         res = 0
         for i in self.list_trips_before_date(date, strict):
             res += i[1].fuel
