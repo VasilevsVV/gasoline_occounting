6177abe8aa410c449b93f5409cbec27e675e664c
Vasiliy <morfey.rulit@gmail.com>
2017-06-18 22:55:59 +0300

    Added different methods of serialization to serialize.py

--- gas_accounting/serialize.py
+++ gas_accounting/serialize.py
@@ -1,5 +1,7 @@
 import pickle as p
 import os
+from json_serialize import JsonSerialize as js
+import yaml
 
 
 class Serialize:
@@ -8,38 +10,87 @@ class Serialize:
     json = 3
 
     @staticmethod
-    def dump(table_name, data, method):
+    def file_name(method):
+        home = dict(os.environb)[b'HOME']
         if method == Serialize.pickle:
-            with open(dict(os.environb)[b'HOME'] + b'/.gas_storage.pickle', 'wb+') as f:
-                try:
-                    table = p.load(f)
-                except:
-                    table = {}
-                table[table_name] = data
-                p.dump(table, f)
-                f.close()
+            file = b'/.gas_storage.pickle'
+        elif method == Serialize.yaml:
+            file = b'/.gas_storage.yaml'
+        else:
+            file = b'/.gas_storage.json'
+        return home + file
+
+    @staticmethod
+    def file_spec_w(method):
+        if method == Serialize.pickle:
+            return 'wb'
+        else:
+            return 'w'
+
+    @staticmethod
+    def file_spec_r(method):
+        if method == Serialize.pickle:
+            return 'rb'
+        else:
+            return 'r'
+
+    @staticmethod
+    def read_file(method):
+        return open(Serialize.file_name(method),
+                    Serialize.file_spec_r(method))
+
+    @staticmethod
+    def write_file(method):
+        return open(Serialize.file_name(method),
+                    Serialize.file_spec_w(method))
+
+    @staticmethod
+    def dump(table_name, data, method):
+        table = Serialize.load_all(method)
+        table[table_name] = data
+        Serialize.dump_all(table, method)
+
+    @staticmethod
+    def dump_all(data, method):
+        with Serialize.write_file(method) as f:
+            if method == Serialize.pickle:
+                p.dump(data, f)
+            elif method == Serialize.yaml:
+                yaml.dump(data, f)
+            else:
+                js.dump(data, f)
+            f.close()
+
+    @staticmethod
+    def load_all(method):
+        if method == Serialize.pickle:
+            try:
+                with Serialize.read_file(method) as f:
+                    if method == Serialize.pickle:
+                        table = p.load(f)
+                    elif method == Serialize.yaml:
+                        table = yaml.load(f)
+                    else:
+                        table = js.load(f)
+                    f.close()
+            except:
+                table = {}
+            if table is None:
+                table = {}
+            return table
 
     @staticmethod
     def load(table_name, method):
-        if method == Serialize.pickle:
-            with open(dict(os.environb)[b'HOME'] + b'/.gas_storage.pickle', 'wb+') as f:
-                try:
-                    table = p.load(f)
-                except:
-                    table = {}
-            return table.get(table_name, [{}, 0])
+        table = Serialize.load_all(method)
+        return table.get(table_name, [{}, 0])
 
     @staticmethod
     def delete(table_name, method):
-        if method == Serialize.pickle:
-            with open(dict(os.environb)[b'HOME'] + b'/.gas_storage.pickle', 'wb+') as f:
-                try:
-                    table = p.load(f)
-                    if table.get(table_name, False):
-                        table.pop(table_name)
-                        p.dump(table, f)
-                        return True
-                    else:
-                        return False
-                except:
-                    return False
+        table = Serialize.load_all(method)
+        try:
+            table.pop(table_name)
+            Serialize.dump_all(table, method)
+            return True
+        except:
+            return False
+
