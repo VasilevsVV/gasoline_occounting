b72647525a9289be4584846d2d6b59817afca5a4
Vasiliy <morfey.rulit@gmail.com>
2017-06-21 21:26:24 +0300

    Added methods detailed_info, _query, current_product_list & total.

--- calories_manager/Model.py
+++ calories_manager/Model.py
@@ -25,7 +25,6 @@ class Model():
             raise Exception("Activity should be one of these values: 1.2, 1.375, 1.55, 1.7, 1.9.")
         User(userName=name, age=age, height=height, weight=weight, gender=gender, activity=actv)
 
-
     def update_user_param(self, kwargs):
         """ Update user """
         if not self._is_user_exists():
@@ -38,13 +37,11 @@ class Model():
         user.gender = kwargs['gender'] if 'gender' in kwargs.keys() else user.gender
         user.activity = kwargs['activity'] if 'activity' in kwargs.keys() else user.activity
 
-
     def add_product(self, name, energy_points):
         """ add product to the database """
         now = datetime.datetime.now()
         date = "{}-{}-{}".format(now.year, now.month, now.day)
         Product(productName=name, energyPoints=energy_points, date=date)
-
 
     def remove_product(self, product_name):
         now = datetime.datetime.now()
@@ -54,3 +51,36 @@ class Model():
 
         item = Product.select(Product.q.productName == product_name)[0]
         Product.delete(item.id)
+
+    def detailed_info(self, day):
+        date = "{}-{}-{}".format(day['year'], day['month'], day['day'])
+        items = Product.select(Product.q.date == date)
+        total = self.total(day)
+        return (items, total)
+
+    def _query(self, kwargs):
+        if len(kwargs) == 3:
+            date = "{}-{}-{}".format(kwargs['year'], kwargs['month'], kwargs['day'])
+            products = Product.select(Product.q.date == date)
+        elif "month" in kwargs.keys() and "year" in kwargs.keys():
+            prev_month = datetime.date(kwargs['year'], kwargs['month'], 1) - relativedelta(months=1)
+            next_mounth = datetime.date(kwargs['year'], kwargs['month'], 1) + relativedelta(months=1)
+            products = Product.select(sqlobject.AND(Product.q.date > prev_month,\
+                                     Product.q.date < next_mounth))
+        elif "year" in kwargs.keys():
+            now = datetime.datetime.now()
+            products = Product.select(sqlobject.AND(Product.q.date > datetime.date(kwargs['year'] -1, 12, 31),\
+                                     Product.q.date < datetime.date(kwargs['year'] + 1, 1, 1)))
+        return products
+
+    def current_product_list(self):
+        now = datetime.datetime.now()
+        return self._query(dict((("year", now.year), ("month", now.month), ("day", now.day))))
+
+    def total(self, kwargs):
+        products = self._query(kwargs)
+        if list(products) == []:
+            raise Exception("Sorry, but you do not have such records.")
+        return reduce(lambda x, y: x.energyPoints + y.energyPoints, products) if len(list(products)) > 1\
+                                                                            else products[0].energyPoints
+
