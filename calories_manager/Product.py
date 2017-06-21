6bd59b3520bb3be011c6849ef85382ca4a10d47b
Natalia <natalia.chehunina@yandex.ua>
2017-06-21 20:34:30 +0300

    added realization of sql object product

---
+++ calories_manager/Product.py
@@ -0,0 +1,13 @@
+from Connection import *
+
+class Product(sqlobject.SQLObject):
+    _connection = establish_connection()
+    productName = sqlobject.StringCol(length=100)
+    energyPoints = sqlobject.IntCol(default=None)
+    # date = sqlobject.DateCol()
+    date = sqlobject.StringCol()
+    def __repr__(self):
+        return "date: {} name = {}, energy = {}".format(self.date,\
+                                                self.productName, self.energyPoints)
+
+Product.createTable(ifNotExists=True)
