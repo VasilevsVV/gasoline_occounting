991c9bddf77b384c0e737670d68a4bb6a6c8edd2
Natalia <natalia.chehunina@yandex.ua>
2017-06-21 20:29:20 +0300

    added docstrings

--- calories_manager/Connection.py
+++ calories_manager/Connection.py
@@ -1,8 +1,11 @@
+""" Module that provides conn object for the model """
+
 import os
 import sqlobject
 from configuration.configParser import *
 
 def establish_connection():
+    """ Connect to the database """
     res_dict = config_parser_result()
     conn None
     if res_dict[save_type_key] == "mysql":
